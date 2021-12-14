fec8bc6789edb0737dc22ee2bccd37bd0f075e24
Mehrankhan70 <mehrankhanciit@gmail.com>
2021-12-14 16:50:27 +0500

    adding bulk delete and bulk revert

--- src/components/todo/Todo.jsx
+++ src/components/todo/Todo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from './todo.module.css'
 
 const Todo = () => {
@@ -11,6 +11,9 @@ const Todo = () => {
     const [edit, setEdit] = useState(0);
     const [change, setChange] = useState(false)
 
+    useEffect(() => {
+        console.log('somthing in the list...')
+    }, [list])
     // for getting data
     const handleChange=(e) => {
         const task = e.target.value;
@@ -30,21 +33,24 @@ const Todo = () => {
         }
         else
         {
+            const prevVal =  values.filter((val)=>val.title !==  list[edit].title );
+            setValues(prevVal)
             list[edit]=task;
+           
             setTask({title:''});
             setEdit(0);
             setChange(false);
         }
         
     }
-    // getting checbox values
+    // getting checbox values of task of users
     const submitValues=(e,index,title)=>{
         var test = e.target.checked;
         if(test===true) {
             console.log('checkbox value',list[index],'event',e.target.checked);
             var temp = list[index];
             const prevVal = [...values];
-            prevVal.push(temp);
+            prevVal.push({...temp,value:false});
             setValues(prevVal)
             const updatedList = list.map((item, i) => {
                 if(i===index){
@@ -74,20 +80,58 @@ const Todo = () => {
         }
         
     }
+    // getting checbox values of revert users
+    const submitRevert=(e,index,title)=>{
+        var test = e.target.checked;
+        if(test===true) {
+            console.log('checkbox value',list[index],'event',e.target.checked);
+            var temp = checkbox[index];
+            const prevVal = [...values];
+            prevVal.push({...temp,value:false});
+            setValues(prevVal)
+            const updatedList = checkbox.map((item, i) => {
+                if(i===index){
+                    return {
+                        ...item,
+                        value:true
+                    }
+                }
+                return item
+            })
+            setCheckbox(updatedList);
+            console.log('values is',values);
+        }
+        else{
+            const updateCheck = values.filter((val)=>val.title!==title);
+            setValues(updateCheck);
+            const updatedList = checkbox.map((item, i) => {
+                if(i===index){
+                    return {
+                        ...item,
+                        value:false
+                    }
+                }
+                return item
+            })
+            setCheckbox(updatedList);
+        }
+        
+    }
     // submiting checkbox values
     const submitCheckbox =()=>{
         console.log('submitCheckbox',values);
         // setcheckbox.push(values);
         const updatedList = list.filter((item,i)=>!values.some(val=>val.title===item.title))
         setList(updatedList);
-        setCheckbox([...values]);
+        setCheckbox([...checkbox,...values]);
+        setValues([])
         //setList([]);
     }
     // fot deleting data
-    const handleDelete=(index) => {
-        var temp = [...list];
-        temp.splice(index, 1);
-        setList([...temp]);
+    const handleDelete=() => {
+        const updatedList = list.filter((item,i)=>!values.some(val=>val.title===item.title))
+        setList(updatedList);
+        setValues([]);
     }
     //for editing submitData
     const handleEdit=(index) => {
@@ -103,23 +147,21 @@ const Todo = () => {
 
     }
     //for revert data
-    const handleRevert =(index)=>{
-        console.log('revert check',checkbox[index])
-        const temp = checkbox[index];
-        const prevList = [...list]
-        prevList.push(temp)
-        setList(prevList)
-        
-        var del = [...checkbox]
-        del.splice(index, 1);
-        setCheckbox([...del]);
+    const handleRevert =()=>{
+        console.log('submitCheckbox',values);
+        // setcheckbox.push(values);
+        const updatedList = checkbox.filter((item,i)=>!values.some(val=>val.title===item.title))
+        setCheckbox(updatedList);
+        setList([...list,...values]);
+        setValues([])
+
     }
     return (
         <div>
             <div className={styles.input_data}>
                 <h1>Todo List</h1>
                 <input type="text" name="task" value={task.title && task.title} onChange={handleChange} placeholder="Enter your Task here..." id={styles.task} />
-                <button className={styles.submit_btn} onClick={submitData}>Add Task</button>
+                <button className={styles.submit_btn} onClick={submitData}>{change?'Save Data':'Add Task'}</button>
             </div>
 
             <div className={styles.user_data}>
@@ -133,20 +175,26 @@ const Todo = () => {
                         </thead>
                         <tbody>
                             {
+                                list.length > 0?
                             list?.map((item, index) =>{
                                 return(
+                                
                                     <tr key={index}>
                                         <td> <input onChange={(e)=>submitValues(e,index,item.title)} checked={item.value} type="checkbox" /> {item.title} </td>
                                         <td className={styles.action_btns}> 
-                                            <button onClick={() =>handleDelete(index)} className={styles.del_btn}>Delete</button> 
                                             <button onClick={() =>handleEdit(index)} className={styles.edit_btn}>Edit</button>
-                                            {/* <button onClick={() =>submitValues(index)} className={styles.edit_btn}>Submit</button> */}
                                         </td>
                                     </tr>
                                 )
                                 })
-                            }
-                            <button onClick={submitCheckbox} className={styles.submit_btn} >Submit</button>
+                                : <div className={styles.noData}>
+                                    <img src='/noData.svg' alt='no data' />
+                                </div> }
+                           { list.length>0 && <div className={styles.main_btns}>
+                                <button onClick={submitCheckbox} className={styles.submit_btn} >Submit</button>
+                                <button onClick={handleDelete} className={styles.del_btn}>Delete</button>
+                            </div>
+                             }
                         </tbody>
                     </table>
                     
@@ -156,24 +204,29 @@ const Todo = () => {
                         <thead>
                             <tr>
                                 <th>Submitted Values</th>
-                                <th>Action</th>
+                                {/* <th>Action</th> */}
                             </tr>
                         </thead>
                         <tbody>
                             {
+                                checkbox.length>0?
                             checkbox?.map((item, index) =>{
                                 
                                 return(
                                     <tr key={index}>
-                                        <td>  {item.title} </td>
-                                        <td className={styles.action_btns}> 
-                                            <button onClick={()=>handleRevert(index)} className={styles.edit_btn}>Revert</button>
-                                        </td>
+                                        <td> <input onChange={(e)=>submitRevert(e,index,item.title)} checked={item.value} type="checkbox" />   {item.title} </td>
+                                       
                                     </tr>
                                 )
                                 })
+                                :<div className={styles.noData}>
+                                    <img src='/noData.svg' alt='no data' />
+                                </div>
                             }
-                            {/* <button className={styles.submit_btn} onClick={submitValues}>Submit</button> */}
+                            {checkbox.length>0 && <div className={styles.main_btns}>
+                                <button onClick={()=>handleRevert()} className={styles.revert_btn}>Revert</button>
+                            </div>}
+                            
                         </tbody>
                     </table>
                     
